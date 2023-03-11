4079141800453d00427905d4aac1f781d9b447f9
Mateo Demedicci <mat4168@gmail.com>
2023-03-10 23:13:34 -0300

    correcciones de la primera pre-entrega

--- index.js
+++ index.js
@@ -1,46 +1,47 @@
 // Proyecto sobre e-commerce de un negocio de discos de vinilo
 
-class disco {
+class Disco {
     constructor(nombre, precio) {
         this.nombre = nombre
         this.precio = precio
     }
 }
+function calcularCuotas(){
+    return Math.round(discoPrecio / seleccionarCuotas)
+}
 //Discos disponibles
-
-const disco1 = new disco('Abbey Road', 6000)
-const disco2 = new disco('Bicicleta', 8000)
-const disco3 = new disco('Thriller', 5500)
-let seleccionardisco = prompt('Seleccione que disco quiere comprar (los discos disponibles son Abbey Road, Bicicleta y Thriller)')
-let discoseleccionado = false
+const disco1 = new Disco('Abbey Road', 6000)
+const disco2 = new Disco('Bicicleta', 8000)
+const disco3 = new Disco('Thriller', 5500)
+let seleccionarDisco = prompt('Seleccione que disco quiere comprar (los discos disponibles son Abbey Road, Bicicleta y Thriller)')
+let discoSeleccionado = false
 //Seleccionar el disco
-while (discoseleccionado === false) {
-    if (seleccionardisco === 'Abbey Road') {
-        discoseleccionado = true
-        discoprecio = disco1.precio
-    } else if (seleccionardisco === 'Bicicleta') {
-        discoseleccionado = true
-        discoprecio = disco2.precio
-    } else if (seleccionardisco === 'Thriller') {
-        discoseleccionado = true
-        discoprecio = disco3.precio
-    } else seleccionardisco = prompt('disco no disponible: seleccione un disco en venta')
+while (discoSeleccionado === false) {
+    if (seleccionarDisco === 'Abbey Road') {
+        discoSeleccionado = true
+        discoPrecio = disco1.precio
+    } else if (seleccionarDisco === 'Bicicleta') {
+        discoSeleccionado = true
+        discoPrecio = disco2.precio
+    } else if (seleccionarDisco === 'Thriller') {
+        discoSeleccionado = true
+        discoPrecio = disco3.precio
+    } else seleccionarDisco = prompt('disco no disponible: seleccione un disco en venta')
 }
 //Seleccionar cuotas 
-let seleccionarcuotas = +prompt('El total a pagar son $' + discoprecio + ' ¿En cuantas cuotas quiere realizar el pago?')
-let cuotaseleccionada = false
-
-while (cuotaseleccionada === false) {
-    if (seleccionarcuotas=== 1){
-        alert('Se realizará un solo pago de $'+discoprecio)
-        cuotaseleccionada = true
+let seleccionarCuotas = parseInt (prompt('El total a pagar son $' + discoPrecio + ' ¿En cuantas cuotas quiere realizar el pago?'))
+let cuotaSeleccionada = false
+while (cuotaSeleccionada === false) {
+    if (seleccionarCuotas=== 1){
+        alert('Se realizará un solo pago de $'+discoPrecio)
+        cuotaSeleccionada = true
         break
     }
-     else if (seleccionarcuotas >= 2 && seleccionarcuotas <= 12) {
-        alert('se realizara el pago en ' + seleccionarcuotas + ' cuotas de $' + Math.round(discoprecio / seleccionarcuotas))
-        cuotaseleccionada = true
+     else if (seleccionarCuotas >= 2 && seleccionarCuotas <= 12) {
+        alert('se realizara el pago en ' + seleccionarCuotas + ' cuotas de $' + calcularCuotas())
+        cuotaSeleccionada = true
         break
     }
-    else seleccionarcuotas = prompt('Los pagos solo estan disponibles hasta 12 cuotas, seleccione un número de cuotas valido')
+    else seleccionarCuotas = prompt('Los pagos solo estan disponibles hasta 12 cuotas, seleccione un número de cuotas valido')
 }
 
