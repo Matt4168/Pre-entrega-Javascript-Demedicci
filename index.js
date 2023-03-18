5e4477c1a9d44a9308782ff5101beeb611300433
Mateo Demedicci <mat4168@gmail.com>
2023-03-18 01:19:52 -0300

    segunda pre-entrega con arrays

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 // Proyecto sobre e-commerce de un negocio de discos de vinilo
-
 class Disco {
     constructor(nombre, precio) {
         this.nombre = nombre
@@ -7,33 +6,57 @@ class Disco {
     }
 }
 function calcularCuotas(){
-    return Math.round(discoPrecio / seleccionarCuotas)
+    return Math.round(total/ seleccionarCuotas)
 }
-//Discos disponibles
-const disco1 = new Disco('Abbey Road', 6000)
-const disco2 = new Disco('Bicicleta', 8000)
-const disco3 = new Disco('Thriller', 5500)
-let seleccionarDisco = prompt('Seleccione que disco quiere comprar (los discos disponibles son Abbey Road, Bicicleta y Thriller)')
-let discoSeleccionado = false
-//Seleccionar el disco
-while (discoSeleccionado === false) {
-    if (seleccionarDisco === 'Abbey Road') {
-        discoSeleccionado = true
-        discoPrecio = disco1.precio
-    } else if (seleccionarDisco === 'Bicicleta') {
-        discoSeleccionado = true
-        discoPrecio = disco2.precio
-    } else if (seleccionarDisco === 'Thriller') {
-        discoSeleccionado = true
-        discoPrecio = disco3.precio
-    } else seleccionarDisco = prompt('disco no disponible: seleccione un disco en venta')
+//Array de discos disponibles
+const discos = [
+    new Disco('abbey road', 6000),
+    new Disco('bicicleta', 8000),
+    new Disco('thriller', 5500),
+    new Disco ('apostrophe', 10000)
+]
+console.log(discos)
+let seleccionarDisco = prompt('Seleccione que disco quiere comprar (los discos disponibles son Abbey Road, Bicicleta, Thriller y Apostrophe)')
+let seguirComprando = true
+const carritoDeCompras = []
+while (seguirComprando === true) {
+    const disco = discos.find(
+        (disco) => disco.nombre === seleccionarDisco.toLowerCase().trim()
+      )
+      if (disco) {
+        carritoDeCompras.push(disco)
+      } else {
+        seleccionarDisco = prompt(
+          'Elija un disco disponible (Abbey Road, Bicicleta, Thriller y Apostrophe))'
+        )
+        continue
+      }
+
+    let elegir = prompt('Deseas seguir comprando? Si/No')
+    let respuestaValida = true
+    while (respuestaValida === true){
+        if (elegir.toLowerCase().trim() === 'si') {
+            seleccionarDisco = prompt(
+                'los discos disponibles son Abbey Road, Bicicleta, Thriller y Apostrophe'),
+                respuestaValida = false
+            } else if (elegir.toLowerCase().trim() === 'no'){
+                respuestaValida = false
+                seguirComprando = false
+            }
+            else{
+                elegir = prompt ('seleccione una respuesta valida.  Si/No ' )
+            }
+    }
+ 
 }
-//Seleccionar cuotas 
-let seleccionarCuotas = parseInt (prompt('El total a pagar son $' + discoPrecio + ' ¿En cuantas cuotas quiere realizar el pago?'))
+let total = 0
+carritoDeCompras.forEach(disco=>{total = total+disco.precio})
+// Seleccionar y calcular cuotas
+let seleccionarCuotas = parseInt (prompt('El total a pagar son $' + total + ' ¿En cuantas cuotas quiere realizar el pago?'))
 let cuotaSeleccionada = false
 while (cuotaSeleccionada === false) {
     if (seleccionarCuotas=== 1){
-        alert('Se realizará un solo pago de $'+discoPrecio)
+        alert('Se realizará un solo pago de $'+total)
         cuotaSeleccionada = true
         break
     }
