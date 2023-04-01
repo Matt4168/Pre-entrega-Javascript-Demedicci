919ec32d71dfa9704a72ce77a4b729071878f0c4
Mateo Demedicci <mat4168@gmail.com>
2023-04-01 14:39:31 -0300

    3ra pre-entrega

--- index.js
+++ index.js
@@ -1,70 +1,110 @@
 // Proyecto sobre e-commerce de un negocio de discos de vinilo
+const formulario = document.getElementById('formulario')
+const ingreseNombre = document.getElementById('nombre')
+const ingreseApellido = document.getElementById('apellido')
+const titulo = document.getElementById('titulo')
+
+// formulario de ingreso
+formulario.onsubmit = (e) => {
+  e.preventDefault()
+  const infoUsuario = {
+    nombre: ingreseNombre.value,
+    apellido: ingreseApellido.value,
+  }
+  localStorage.setItem('infoUsuario', JSON.stringify(infoUsuario))
+  formulario.remove()
+  titulo.innerText = `Hola ${infoUsuario.nombre} ${infoUsuario.apellido}`
+}
+
+const infoUsuario = localStorage.getItem('infoUsuario')
+const infoUsuarioJS = JSON.parse(infoUsuario)
+if (infoUsuario) {
+  formulario.remove()
+  titulo.innerText = `Hola ${infoUsuarioJS.nombre} ${infoUsuarioJS.apellido}`
+}
+
+
 class Disco {
-    constructor(nombre, precio) {
+    constructor(id, nombre, precio, imagen) {
+        this.id = id 
         this.nombre = nombre
         this.precio = precio
+        this.imagen = imagen
     }
 }
-function calcularCuotas(){
-    return Math.round(total/ seleccionarCuotas)
-}
-//Array de discos disponibles
-const discos = [
-    new Disco('abbey road', 6000),
-    new Disco('bicicleta', 8000),
-    new Disco('thriller', 5500),
-    new Disco ('apostrophe', 10000)
-]
-console.log(discos)
-let seleccionarDisco = prompt('Seleccione que disco quiere comprar (los discos disponibles son Abbey Road, Bicicleta, Thriller y Apostrophe)')
-let seguirComprando = true
-const carritoDeCompras = []
-while (seguirComprando === true) {
-    const disco = discos.find(
-        (disco) => disco.nombre === seleccionarDisco.toLowerCase().trim()
-      )
-      if (disco) {
-        carritoDeCompras.push(disco)
-      } else {
-        seleccionarDisco = prompt(
-          'Elija un disco disponible (Abbey Road, Bicicleta, Thriller y Apostrophe))'
-        )
-        continue
-      }
 
-    let elegir = prompt('Deseas seguir comprando? Si/No')
-    let respuestaValida = true
-    while (respuestaValida === true){
-        if (elegir.toLowerCase().trim() === 'si') {
-            seleccionarDisco = prompt(
-                'los discos disponibles son Abbey Road, Bicicleta, Thriller y Apostrophe'),
-                respuestaValida = false
-            } else if (elegir.toLowerCase().trim() === 'no'){
-                respuestaValida = false
-                seguirComprando = false
-            }
-            else{
-                elegir = prompt ('seleccione una respuesta valida.  Si/No ' )
-            }
+const discos = [
+    new Disco(1,'Abbey Road', 9000, './images/abbey road.jpg'),
+    new Disco(2,'The dark side of the Moon', 7500, './images/The_Dark_Side_of_the_Moon_Cover.svg.png'),
+    new Disco(3,'Thriller', 5000, './images/Michael_Jackson_-_Thriller.png'),
+    new Disco(4,'Outlandos D´amour', 8000, './images/thepolice.jpg'),
+    new Disco(5,'Songs in the Key of Life', 15000, './images/Songs_in_the_key_of_life.jpg'),
+    new Disco(6,'Ziggy Stardust', 10000, './images/ZiggyStardust.jpg'),
+    new Disco(7,'La Grasa de las Zapitales', 18000, './images/Serú_Girán_–_Grasa_De_Las_Capitales.jpg'),
+    new Disco(8,'Breakfast in America', 6000, './images/Supertramp_-_Breakfast_in_America.jpg')
+]
+
+let listadoDiscos = document.getElementById('listadoDiscos')
+
+    discos.forEach(disco =>{
+        listadoDiscos.innerHTML += `<div class="card" style="width: 18rem;">
+        <img src="${disco.imagen}" class="card-img-top" alt="${disco.nombre}">
+        <div class="card-body">
+          <h5 class="card-title">${disco.nombre}</h5>
+          <p class="card-text">$${disco.precio}</p>
+          <a href="#" id=${disco.id} class="btn btn-primary">Comprar</a>
+        </div>
+      </div>`
     }
- 
-}
-let total = 0
-carritoDeCompras.forEach(disco=>{total = total+disco.precio})
-// Seleccionar y calcular cuotas
-let seleccionarCuotas = parseInt (prompt('El total a pagar son $' + total + ' ¿En cuantas cuotas quiere realizar el pago?'))
-let cuotaSeleccionada = false
-while (cuotaSeleccionada === false) {
-    if (seleccionarCuotas=== 1){
-        alert('Se realizará un solo pago de $'+total)
-        cuotaSeleccionada = true
-        break
+)
+
+const carrito = []
+
+const botonesAgregar = document.querySelectorAll('.btn-primary')
+
+botonesAgregar.forEach((boton) => {
+  boton.onclick = () => {
+    const disco = discos.find((disco) => disco.id === parseInt(boton.id))
+    
+    const discosCarrito = {
+      id: disco.id,
+      nombre: disco.nombre,
+      precio: disco.precio,
+      cantidad: 1,
     }
-     else if (seleccionarCuotas >= 2 && seleccionarCuotas <= 12) {
-        alert('se realizara el pago en ' + seleccionarCuotas + ' cuotas de $' + calcularCuotas())
-        cuotaSeleccionada = true
-        break
+  const discoEnCarrito = carrito.find(disco=>disco.id===discosCarrito.id)
+    if(!discoEnCarrito){
+        carrito.push(discosCarrito)
+    } else {
+        discoEnCarrito.cantidad++
     }
-    else seleccionarCuotas = prompt('Los pagos solo estan disponibles hasta 12 cuotas, seleccione un número de cuotas valido')
+  }
+})
+
+const botonFinalizarCompra = document.querySelector('#fin')
+const thead = document.querySelector('#thead')
+const tbody = document.querySelector('#tbody')
+const parrafoTotal = document.querySelector('#total')
+botonFinalizarCompra.onclick = ()=>{
+    listadoDiscos.remove()
+    botonFinalizarCompra.remove()
+    thead.innerHTML = `             <tr>
+    <th scope="col">Producto</th>
+    <th scope="col">Cantidad</th>
+    <th scope="col">Total</th>
+  </tr>`
+
+let totalCompra = 0
+carrito.forEach(disco=>{
+    totalCompra+= disco.cantidad*disco.precio
+    tbody.innerHTML+=`
+    <tr>
+      <td>${disco.nombre}</td>
+      <td>${disco.cantidad}</td>
+      <td>$${disco.cantidad*disco.precio}</td>
+    </tr>
+    `
+})
+parrafoTotal.innerHTML = `El total a pagar es $${totalCompra}`
 }
 
