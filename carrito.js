6db2cdd33986010f028a35e0afa40275d2c4f4fa
Mateo Demedicci <mat4168@gmail.com>
2023-04-18 21:44:19 -0300

    Entrega final

--- carrito.js
+++ carrito.js
@@ -1,8 +1,45 @@
 let productosEnCarrito = JSON.parse(localStorage.getItem("carrito"))
+let datos = JSON.parse(localStorage.getItem("infoUsuario"))
 const contenedorCarrito = document.getElementById('contenedorCarrito')
 const parrafoTotal = document.getElementById('total')
-const fin = document.getElementById('fin')
 let totalCompra = 0
+
+function eliminarProducto(){
+    const botonEliminar = document.querySelectorAll(".carrito-producto-eliminar");
+	botonEliminar.forEach((boton) => {
+		boton.onclick = () => {
+			const discoIndex = productosEnCarrito.findIndex((disco) => disco.id === boton.id);
+			console.log(discoIndex);
+			if (productosEnCarrito[discoIndex].cantidad === 1) {
+				productosEnCarrito.splice(discoIndex, 1);
+			} else {
+				productosEnCarrito[discoIndex].cantidad--;
+			}
+            Toastify({
+                    text: "Producto eliminado",
+                    duration: 3000,
+                    close: true,
+                    gravity: "top", // `top` or `bottom`
+                    position: "right", // `left`, `center` or `right`
+                    stopOnFocus: true, // Prevents dismissing of toast on hover
+                    style: {
+                      background: "linear-gradient(to right, #4b33a8, #785ce9)",
+                      borderRadius: "2rem",
+                      textTransform: "uppercase",
+                      fontSize: ".75rem"
+                    },
+                    offset: {
+                        x: '1.5rem', // horizontal axis - can be a number or a string indicating unity. eg: '2em'
+                        y: '1.5rem' // vertical axis - can be a number or a string indicating unity. eg: '2em'
+                      },
+                    onClick: function(){} // Callback after click
+                  }).showToast();
+			localStorage.setItem("carrito", JSON.stringify(productosEnCarrito));
+			actualizarCarrito();
+		};
+	});
+}
+
 const actualizarCarrito = () => {
   contenedorCarrito.innerHTML = "";
   totalCompra = 0; 
@@ -21,62 +58,33 @@ const actualizarCarrito = () => {
                 <p class="card-text">${disco.nombre}</p>
                 <p class="card-text">${disco.cantidad}</p>
                 <p class="card-text">$${disco.cantidad*disco.precio}</p>
-                <button type="button" class="carrito-producto-eliminar" id="borrar-${disco.id}">Eliminar</button>
+                <button type="button" class="carrito-producto-eliminar" id="${disco.id}" onclick="eliminarProducto(${disco.id})">Eliminar</button>
+
               </div>
             </div>
           </div>
         </div>
       `;
     });
-    parrafoTotal.innerHTML = `El total a pagar es $${totalCompra}
-    <button id="fin">Finalizar Compra</button>`
+    parrafoTotal.innerHTML = `El total a pagar es $${totalCompra}, Sr/Sra ${datos.apellido}
+    <button type="button" class="finalizar" onclick="finalizarCompra()">Finalizar Compra</button>
+    `
     ; 
-    const eliminar = document.querySelectorAll(".carrito-producto-eliminar");
-    for (let i = 0; i < eliminar.length; i++) {
-      eliminar[i].addEventListener("click", () => {
-        eliminarProducto(eliminar[i].id.split("-")[1]); 
-      });
-    }
+
   } else {
     parrafoTotal.innerHTML = `Seleccione productos para comprar`
   }
+  
 }
+actualizarCarrito();
 
-const eliminarProducto = (id) => {
-  const idBuscado = parseInt(id);
-  const indiceObjeto = productosEnCarrito.findIndex((disco) => disco.id === idBuscado);
-  if (indiceObjeto !== -1) { 
-    productosEnCarrito.splice(indiceObjeto, 1); 
-    localStorage.setItem("carrito", JSON.stringify(productosEnCarrito)); 
-    actualizarCarrito();
-  }
-  Toastify({
-    text: "Producto eliminado",
-    duration: 3000,
-    close: true,
-    gravity: "top", // `top` or `bottom`
-    position: "right", // `left`, `center` or `right`
-    stopOnFocus: true, // Prevents dismissing of toast on hover
-    style: {
-      background: "linear-gradient(to right, #4b33a8, #785ce9)",
-      borderRadius: "2rem",
-      textTransform: "uppercase",
-      fontSize: ".75rem"
-    },
-    offset: {
-        x: '1.5rem', // horizontal axis - can be a number or a string indicating unity. eg: '2em'
-        y: '1.5rem' // vertical axis - can be a number or a string indicating unity. eg: '2em'
-      },
-    onClick: function(){} // Callback after click
-  }).showToast();
+function finalizarCompra(){
+    Swal.fire({
+        icon: 'success',
+        title: 'Muchas Gracias por su Compra!',
+        text: 'El total a pagar es de $'+totalCompra,
+        footer: '<a href="https://www.mercadopago.com.ar/">Proceder al pago</a>'
+      })
 
 }
-// actualizarCarrito();
-// fin.addEventListener("click", finalizar);
-// function finalizar() {
-//     Swal.fire(
-//         'Good job!',
-//         'You clicked the button!',
-//         'success'
-//       )
-// }
+
--- index.js
+++ index.js
@@ -23,80 +23,53 @@ if (infoUsuario) {
   titulo.innerText = `Hola ${infoUsuarioJS.nombre} ${infoUsuarioJS.apellido}`
 }
 
+let discos = [];
+fetch("discos.json")
+	.then((Response) => Response.json())
+	.then((data) => {
+		discos = data;
+		cargarDiscos(discos);
 
+		cargarCarrito();
+	});
 
-// class Disco {
-//     constructor(id, nombre, precio, imagen) {
-//         this.id = id
-//         this.nombre = nombre
-//         this.precio = precio
-//         this.imagen = imagen
-//     }
-// }
-
-// const discos = [
-//     new Disco(1,'Abbey Road', 9000, './images/ar.jpg'),
-//     new Disco(2,'The dark side of the Moon', 7500, './images/The_Dark_Side_of_the_Moon_Cover.svg.png'),
-//     new Disco(3,'Thriller', 5000, './images/Michael_Jackson_-_Thriller.png'),
-//     new Disco(4,'Outlandos D´amour', 8000, './images/thepolice.jpg'),
-//     new Disco(5,'Songs in the Key of Life', 15000, './images/Songs_in_the_key_of_life.jpg'),
-//     new Disco(6,'Ziggy Stardust', 10000, './images/ZiggyStardust.jpg'),
-//     new Disco(7,'La Grasa de las Zapitales', 18000, './images/Serú_Girán_–_Grasa_De_Las_Capitales.jpg'),
-//     new Disco(8,'Breakfast in America', 6000, './images/Supertramp_-_Breakfast_in_America.jpg')
-// ]
-
-let discos = []
-fetch('discos.json')
-.then(Response => Response.json())
-.then(data =>{
-  discos = data;
-  cargarDiscos(discos)
-})
-
-
-
-function cargarDiscos(){
-  let listadoDiscos = document.getElementById('listadoDiscos')
-  discos.forEach(disco =>{
-      listadoDiscos.innerHTML += `<div class="card" style="width: 18rem;">
+function cargarDiscos(discos) {
+	let listadoDiscos = document.getElementById("listadoDiscos");
+	discos.forEach((disco) => {
+		listadoDiscos.innerHTML += `<div class="card" style="width: 18rem;">
       <img src="${disco.imagen}" class="card-img-top" alt="${disco.nombre}">
       <div class="card-body">
         <h5 class="card-title">${disco.nombre}</h5>
         <p class="card-text">$${disco.precio}</p>
-        <a href="#" id=${disco.id} class="btn btn-primary">Comprar</a>
+        <button id=${disco.id} class="btn btn-primary">Comprar</button>
       </div>
-    </div>`
-  }
-)
+    </div>`;
+	});
 }
-cargarDiscos()
-const carrito = []
-const botonesAgregar = document.querySelectorAll('.btn-primary')
-cargarCarrito(carrito)
-function cargarCarrito(){
-  botonesAgregar.forEach((boton) => {
-    boton.onclick = () => {
-      const disco = discos.find((disco) => disco.id === parseInt(boton.id))
-      const discosCarrito = {
-        id: disco.id,
-        nombre: disco.nombre,
-        precio: disco.precio,
-        imagen: disco.imagen,
-        cantidad: 1,
-      }
-    const discoEnCarrito = carrito.find(disco=>disco.id===discosCarrito.id)
-      if(!discoEnCarrito){
-          carrito.push(discosCarrito)
-      } else {
-          discoEnCarrito.cantidad++
-      }
-      localStorage.setItem("carrito", JSON.stringify(carrito))
-    }
-  
-  })
+// cargarDiscos();
+const carrito = [];
+function cargarCarrito() {
+	const botonesAgregar = document.querySelectorAll(".btn-primary");
+	botonesAgregar.forEach((boton) => {
+		boton.onclick = () => {
+			const disco = discos.find((disco) => disco.id === boton.id);
+			console.log(disco);
+			const discosCarrito = {
+				id: disco.id,
+				nombre: disco.nombre,
+				precio: disco.precio,
+				imagen: disco.imagen,
+				cantidad: 1,
+			};
+			const discoEnCarrito = carrito.find((disco) => disco.id === discosCarrito.id);
+			if (!discoEnCarrito) {
+				carrito.push(discosCarrito);
+			} else {
+				discoEnCarrito.cantidad++;
+			}
+			localStorage.setItem("carrito", JSON.stringify(carrito));
+		};
+	});
 }
 
-
-
-
-
+console.log(carrito)
